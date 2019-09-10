f3adb3970b91a6fd07a96086a9aee7ac06b60d91
Kevin Tong <38227264+meanderer-tech@users.noreply.github.com>
2019-09-10 14:19:30 -0400

    now install recommends.

--- frame-setup.sh
+++ frame-setup.sh
@@ -3,7 +3,7 @@ apt -y update
 apt -y upgrade
 
 apt install -y lightdm
-apt install -y --no-install-recommends xserver-xorg
-apt install -y --no-install-recommends xinit
-apt install -y --no-install-recommends raspberrypi-ui-mods lxsession pi-greeter rpd-icons gtk2-engines-clearlookspix
+apt install -y xserver-xorg
+apt install -y xinit
+apt install -y raspberrypi-ui-mods lxsession pi-greeter rpd-icons gtk2-engines-clearlookspix
 apt install -y chromium
