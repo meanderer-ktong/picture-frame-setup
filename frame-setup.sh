069197b1f556fe348b001320c31aa6d4ebc29bc5
Kevin Tong <38227264+meanderer-tech@users.noreply.github.com>
2019-09-10 15:29:45 -0400

    add TODO

--- frame-setup.sh
+++ frame-setup.sh
@@ -6,4 +6,15 @@ apt install -y lightdm
 apt install -y xserver-xorg
 apt install -y xinit
 apt install -y raspberrypi-ui-mods lxsession pi-greeter rpd-icons gtk2-engines-clearlookspix
-apt install -y chromium
+apt install -y chromium-browser
+
+# TO DO
+# ask user if ssh to be enabled, and if enabled ask for password
+# enable vnc 
+# set locale
+# set timezone
+# set keyboard layout
+# set wifi country
+# unset overscan
+
+
