8ca282a1fdafc29b0d6e235be47593a807d25c88
Kevin Tong <38227264+meanderer-tech@users.noreply.github.com>
2019-09-11 01:01:58 -0400

    more updates.

--- frame-setup.sh
+++ frame-setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+read -p "Do you want to enable remote access (ssh and vnc)? "
+if [ "$REPLY" != "${REPLY#[Yy]}" ] ;then
+  passwd
+else
+  echo "Skipping this step"
+fi
+
 apt -y update
 apt -y upgrade
 
@@ -8,18 +16,29 @@ apt install -y xinit
 apt install -y raspberrypi-ui-mods lxsession pi-greeter rpd-icons gtk2-engines-clearlookspix
 apt install -y unclutter
 apt install -y chromium-browser 
-apt install -y openssh-server
-systemctl enable ssh
-apt install -y realvnc-vnc-server
-systemctl enable vnc
+apt install -y python3-gpiozero
+ 
+if [ "$REPLY" != "${REPLY#[Yy]}" ] ;then
+  apt install -y openssh-server
+  systemctl enable ssh
+  apt install -y realvnc-vnc-server
+  systemctl enable vncserver-X11-serviced
+fi
 
 # TO DO
-# ask user if ssh to be enabled, and if enabled ask for password
-# enable vnc 
 # set locale
 # set timezone
 # set keyboard layout
 # set wifi country
 # unset overscan
+
+#disable screen blanking
+# sed -i ~/.xscreensaver "s/mode: */mode: off/"
+
+# modify /etc/xdg/lxsession/LXDE-pi/autostart
+#@xset s noblank
+#@xset s off
+#@xset s -dpms
+#@chromium-browser --start-fullscreen [URL]
 
 
