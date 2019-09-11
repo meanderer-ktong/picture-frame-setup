36770f9798b7ae631b55b22ed6a4ab1a46ba9009
Kevin Tong <38227264+meanderer-tech@users.noreply.github.com>
2019-09-11 14:28:03 -0400

    various fixes

--- frame-setup.sh
+++ frame-setup.sh
@@ -5,58 +5,67 @@ locale=en_US.UTF-8
 layout=us
 country=US
 url=fill-me-in
+timezone='America/New_York'
 
 #prompt for remote access
 read -p "Do you want to enable remote access (ssh and vnc)? "
 if [ "$REPLY" != "${REPLY#[Yy]}" ] ;then
+  echo "Please change your password now"
   passwd
 else
   echo "Skipping this step"
 fi
 
 #install GUI desktop, chromium and python GPIO
-apt -y update
-apt -y upgrade
-apt install -y lightdm
-apt install -y xserver-xorg
-apt install -y xinit
-apt install -y raspberrypi-ui-mods lxsession pi-greeter rpd-icons gtk2-engines-clearlookspix
-apt install -y unclutter
-apt install -y chromium-browser 
-apt install -y python3-gpiozero
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt install -y lightdm
+sudo apt install -y xserver-xorg
+sudo apt install -y xinit
+sudo apt install -y raspberrypi-ui-mods lxsession pi-greeter rpd-icons gtk2-engines-clearlookspix
+sudo apt install -y vim lxterminal unclutter
+sudo apt install -y chromium-browser 
+sudo apt install -y python3-gpiozero
 
 #actually enabling remote access
 if [ "$REPLY" != "${REPLY#[Yy]}" ] ;then
-  apt install -y openssh-server
-  systemctl enable ssh
-  apt install -y realvnc-vnc-server
-  systemctl enable vncserver-X11-serviced
+  sudo apt install -y openssh-server
+  sudo systemctl enable ssh
+  sudo apt install -y realvnc-vnc-server
+  sudo systemctl enable vncserver-x11-serviced
 fi
 
 # set locale
-raspi-config nonint do_change_locale $locale
-raspi-config nonint do_configure_keyboard $layout
-raspi-config nonint do_wifi_country $country
-raspi-config nonint do_boot_behaviour B4
-raspi-config nonint do_overscan 0
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+sudo locale-gen en_US.UTF-8
+sudo raspi-config nonint do_change_locale $locale
+sudo raspi-config nonint do_configure_keyboard $layout
+sudo raspi-config nonint do_wifi_country $country
+sudo raspi-config nonint do_boot_behaviour B4
+sudo raspi-config nonint do_overscan 0
+# set timezone 
+sudo rm /etc/localtime
+sudo ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 
 #disable screen blanking
-sed -i -e '/mode:/ s/:.*/:           off/' /home/pi/.xscreensaver
-xscreensaver-command -restart
+#sed -i -e '/mode:/ s/:.*/:           off/' /home/pi/.xscreensaver
+#xscreensaver-command -restart
+#needs to be done in GUI, when the script is run file and services aren't enabled
 
 # modify /etc/xdg/lxsession/LXDE-pi/autostart
 #@xset s noblank
 #@xset s off
 #@xset s -dpms
+#@sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Local\ State
 #@chromium-browser --start-fullscreen [URL]
 
 #cleanup
-apt autoremove && apt clean
+sudo apt -y --purge autoremove && apt clean
 
-addr=$(ip a | grep "inet " | grep -v "host lo" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n1)
-if [ "$REPLY" != "${REPLY#[Yy]}" ] ;then
-  echo "The IP address is $addr, use this address for remote access"
-fi
+#all done reboot
+sudo reboot
 
 
 
