51dd74e98681721bcce4b483edc474df138491e4
Kevin Tong <38227264+meanderer-tech@users.noreply.github.com>
2019-09-10 21:51:59 -0400

    more edits

--- frame-setup.sh
+++ frame-setup.sh
@@ -6,7 +6,12 @@ apt install -y lightdm
 apt install -y xserver-xorg
 apt install -y xinit
 apt install -y raspberrypi-ui-mods lxsession pi-greeter rpd-icons gtk2-engines-clearlookspix
-apt install -y chromium-browser
+apt install -y unclutter
+apt install -y chromium-browser 
+apt install -y openssh-server
+systemctl enable ssh
+apt install -y realvnc-vnc-server
+systemctl enable vnc
 
 # TO DO
 # ask user if ssh to be enabled, and if enabled ask for password
