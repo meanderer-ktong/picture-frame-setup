0828c1182005ec0d17ae97d529acfaa03e0a0d25
Kevin Tong <38227264+meanderer-tech@users.noreply.github.com>
2019-09-13 00:43:10 -0400

    initial commit

---
+++ rpi-hdmi.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Enable and disable HDMI output on the Raspberry Pi
+# Gist forked from 
+# https://gist.github.com/quelleck/1d8fc8395fd52d4aa38a99ef46d8598e
+
+is_off ()
+{
+	vcgencmd display_power | grep "display_power=0" >/dev/null
+}
+
+case $1 in
+	off)
+        	if is_off
+        	then
+            		echo Already off...
+        	else
+            		vcgencmd display_power 0
+        	fi
+	;;
+	on)
+		if is_off
+		then
+			vcgencmd display_power 1
+        	else
+            		echo Already on...
+		fi
+	;;
+	status)
+		if is_off
+		then
+			echo off
+		else
+			echo on
+		fi
+	;;
+	*)
+		echo "Usage: $0 on|off|status" >&2
+		exit 2
+	;;
+esac
+
+exit 0
